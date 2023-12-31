19ef26a81a37c2f78ea1c3e33f8bb2f291ae9cea
agesset <armandessetcheou@gmail.com>
2023-12-31 19:11:13 +0100

    Check win function

--- array/ticTacToe.js
+++ array/ticTacToe.js
@@ -12,7 +12,7 @@ const winConcditions = [
     [0, 4, 8],
     [2, 4, 6]
 ];
-let options = ["", "", "", "", "", "", "", ""];
+let options = ["", "", "", "", "", "", "", "", ""];
 let currentPlayer = "X";
 let running = false;
 initializeGame();
@@ -37,11 +37,40 @@ function updateCell(cell, index){
     cell.textContent = currentPlayer;
 }
 function changePlayer(){
+    currentPlayer = (currentPlayer =="X") ? "O" : "X";
+    statusText.textContent = `${currentPlayer}'s turn`;
 
 }
 function checkWinner(){
+    let roundWon =  false;
+    for(let i = 0; i < winConcditions.length; i++){
+        const condition = winConcditions[i];
+        const cellA = options[condition[0]];
+        const cellB= options[condition[1]];
+        const cellC = options[condition[2]];
+
+        if(cellA == "" || cellB == "" || cellC == ""){
+            continue;
+        }
+        if(cellA == cellB && cellB ==  cellC){
+            roundWon = true;
+            break;
+        }
+    }
+    if(roundWon){
+        statusText.textContent = `${currentPlayer} wins!`;
+        running = false;
+    }
+    else if (!options.includes("")){
+        statusText.textContent = `Draw!`;
+        running = false;
+    }
+    else{
+        changePlayer();
+    }
+
 
 }
 function restartGame(){
-
+   currentPlayer = "X";
 }
