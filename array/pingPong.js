1d6a61ed5ec28786f42e3630239a52ac9f84deea
agesset <armandessetcheou@gmail.com>
2024-01-04 23:50:24 +0100

    minimal code for the PingPong game

---
+++ array/pingPong.js
@@ -0,0 +1,7 @@
+const gameBoard = document.querySelector("#gameBoard");
+const ctx = gameBoard.getContext("2d");
+const scoreText = document.querySelector("#scoreText");
+const resetBtn = document.querySelector("#resetBtn");
+const gameWidth = gameBoard.width;
+const gameHeight = gameBoard.height;
+const paddleColor = "forestgreen";
