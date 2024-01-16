ccd363154d3846d08b585a69491cedbe99adb44b
agesset <armandessetcheou@gmail.com>
2024-01-16 01:22:52 +0100

    writing function for the pingpong game ♏

--- array/pingPong.js
+++ array/pingPong.js
@@ -4,4 +4,44 @@ const scoreText = document.querySelector("#scoreText");
 const resetBtn = document.querySelector("#resetBtn");
 const gameWidth = gameBoard.width;
 const gameHeight = gameBoard.height;
-const paddleColor = "forestgreen";
+const paddle1Color = "forestgreen";
+const paddle2Color = "blue";
+const paddle1 = [
+    x = 0,
+    y = 0,
+    width = 0,
+    height = 0
+
+];
+const paddle2 = [
+    x = 0,
+    y = 0,
+    width = 0,
+    height = 0
+
+];
+
+function initGame(){
+
+};
+function createBall(){
+
+};
+function moveBall(){
+
+};
+function drawPaddle(){
+    ctx.fillRect(paddle1.x, paddle1.y, paddle1.width, paddle1.height);
+};
+function movePaddle(){
+
+};
+function handleCollision(){
+
+};
+function checkGoal(){
+
+};
+function restartGame(){
+
+};
