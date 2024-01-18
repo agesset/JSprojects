5a7fad1ac8d921beeffecd4b4a1ecd9fefee33a9
agesset <armandessetcheou@gmail.com>
2024-01-18 01:12:04 +0100

    finish writing the drawPaddle function 🖍 and fixing its own bugs  🔒

--- array/pingPong.js
+++ array/pingPong.js
@@ -6,20 +6,22 @@ const gameWidth = gameBoard.width;
 const gameHeight = gameBoard.height;
 const paddle1Color = "forestgreen";
 const paddle2Color = "blue";
-const paddle1 = [
-    x = 0,
-    y = 0,
-    width = 0,
-    height = 0
+const paddleBorder = "black";
+const paddle1 = {
 
-];
-const paddle2 = [
-    x = 0,
-    y = 0,
-    width = 0,
-    height = 0
+    x : 0,
+    y : 0,
+    width : 20,
+    height : 50
+};
+const paddle2 = {
 
-];
+    x : gameWidth - 20,
+    y : gameHeight - 50,
+    width : 20,
+    height : 50
+}
+;
 
 function initGame(){
 
@@ -31,7 +33,14 @@ function moveBall(){
 
 };
 function drawPaddle(){
+    ctx.strokeSyle = paddleBorder;// define the paddle color
+    ctx.fillStyle = paddle1Color;
     ctx.fillRect(paddle1.x, paddle1.y, paddle1.width, paddle1.height);
+    ctx.strokeRect(paddle1.x, paddle1.y, paddle1.width, paddle1.height);
+
+    ctx.fillStyle = paddle2Color;
+    ctx.fillRect(paddle2.x, paddle2.y, paddle2.width, paddle2.height);
+    ctx.strokeRect(paddle2.x, paddle2.y, paddle2.width, paddle2.height);
 };
 function movePaddle(){
 
