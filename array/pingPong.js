c9f8da2c077bf35b1387b87dd526dc616cf72e5f
agesset <armandessetcheou@gmail.com>
2024-02-09 21:31:11 +0100

    caractéristiques ball

--- array/pingPong.js
+++ array/pingPong.js
@@ -7,6 +7,16 @@ const gameHeight = gameBoard.height;
 const paddle1Color = "forestgreen";
 const paddle2Color = "blue";
 const paddleBorder = "black";
+const ballColor = "yellow"
+ball = {
+    x: gameWidth/2,
+    y: gameHeight/2,
+    xDirection : 0,
+    yDirection : 0,
+    radius : 0,
+
+
+}
 const paddle1 = {
 
     x : 0,
@@ -20,18 +30,39 @@ const paddle2 = {
     y : gameHeight - 50,
     width : 20,
     height : 50
-}
-;
+};
+let playerScore1 = 0;
+let playerScore2 = 0;
 
+initGame();
+window.addEventListener("keydown",movePaddle);
+resetBtn.addEventListener("click",restartGame);
+/**
+ * 
+ */
 function initGame(){
 
 };
+/**
+ * 
+ */
 function createBall(){
-
+    ctx.fillStyle = ballColor;
 };
+
+/**
+ * 
+ */
 function moveBall(){
 
 };
+
+function clearBoard(){
+     
+}
+/**
+ * 
+ */
 function drawPaddle(){
     ctx.strokeSyle = paddleBorder;// define the paddle color
     ctx.fillStyle = paddle1Color;
@@ -42,15 +73,31 @@ function drawPaddle(){
     ctx.fillRect(paddle2.x, paddle2.y, paddle2.width, paddle2.height);
     ctx.strokeRect(paddle2.x, paddle2.y, paddle2.width, paddle2.height);
 };
-function movePaddle(){
+
+/**
+ * 
+ */
+function movePaddle(event){
 
 };
+
+/**
+ * 
+ */
 function handleCollision(){
 
 };
+
+/**
+ * 
+ */
 function checkGoal(){
 
 };
+
+/**
+ * 
+ */
 function restartGame(){
 
 };
